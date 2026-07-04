aa2e3a53dae7f38abc2b3d917233de530a56c578
Narayansethi2026 <sethinarayan674@gmail.com>
2026-07-04 14:31:37 +0530

    complet structure

---
+++ first.js
@@ -0,0 +1,28 @@
+//conditinal statements
+// let age = 12;
+// let mode = "light";
+// let color;
+
+// if (mode === "dark"){
+//     color ="black";
+// }
+// if(mode === "light"){
+//     color = "white";
+// }
+// if(num % 2 === 0) {
+//     console.log("even");
+// } else {
+//     console.log("odd");
+// }
+// if(age > 18){
+//     console.log(" you can give the vote");
+// } else{
+//     console.log("sorry you can not give the vote")
+
+// }
+let age = 18;
+ let result = age >= 18 ? "adult" : "no adult";
+ console.log(result);
+
+       
+
