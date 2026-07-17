4c697f364fe6ae1abe39e5287a2966818cd2094c
Narayansethi2026 <sethinarayan674@gmail.com>
2026-07-17 19:59:35 +0530

    learn and practis java script

--- first.js
+++ first.js
@@ -49,6 +49,130 @@
 // }
 //for of
 // for(let itme)
+// console.log("hello")
+// "abc".toUpperCase()
+// [1, 2, 3].Push(push);
+// function myfunction () {
+//     console.log("welcome to my home!");
+//     console.log("we are learning js :)");
+
+// }
+// myfunction();
+// myfunction();
+// myfunction();
+// myfunction();
+// myfunction();
+// function myfunction(mesg) {
+//     //paramenter -> input
+//     console.log(mesg);
+
+// }
+// myfunction("dicipline");
+//sum function 
+// function sum(a, b) {
+//     return a + b;
+
+// }
+// const arrowsum = (a, b) => {
+//     console.log(a + b);
+// }
+// //multiplication function 
+//   function mul(a, b) {
+//   return a * b;
+// }
+// function countvowels(str) {    
+//     for (const char of str) {
+//         console.log(char);
+//     }
+// }
+// function countvowels (str) {
+//     let cout = 0;
+//     for (cout char of str)  {
+//         if(
+//             char === "a"
+
+//         )
+//     }
+// }
+// let name = "narayan";
+// console.log("welcome" + name);
+// let age = 19;
+
+// if(age >= 18){
+// console.log("ohh bada go hya beta");
+// }else{
+//     console.log("are bhai abhi time nahi hua hai");
+// }
+// for(let i=1; i<=115; i++){
+//     console.log(i);
+// ---------------MAP-----------------
+// const numbers = [1,2,3,4,5,6,7,8,9,10];
+// const result = numbers.map((num) => {
+//     return num * 15;
+// });
+// console.log(result);
+// const students = ["narayan", "rajkumar", "amit"];
+// const result = students.map((name) => {
+//     return "hello " + name;
+
+// });
+// console.log(result);
+//--------------------object aary----------------
+// const products = [
+    
+//          {name: "laptop",price:50000 },
+//          { name: "phone", price:2000}
+// ];
+// const result = products.map((product) => {
+//     return product.name;
+
+// })
+// console.log(result);
+// const students = [
+//     { name: "narayan", marks: 90 },
+//     { name: "sairam", marks: 95 },
+//     { name: "rajumar", marks:65 }
+
+// ];
+//-------------practis queshion--18/07/2026-----------
+// let name = "narayan";
+// let age = 19;
+// let city = "surat";
+
+// console.log(name);
+// console.log(age);
+// console.log(city);
+// let num1 = 20;
+// let num2 = 30;
+// let sum = num1 + num2;
+// console.log("sum =", sum)
+// let number = 25;
+// if (number % 2 === 0){
+//     console.log("even number");
+// } else {
+//     console.log("odd number");
+// }
+// let number = 32;
+// if (number > 0) {
+//     console.log("postive");
+// } else if (number < 0) {
+//     console.log("nagative");
+// } else {
+//     console.log("zero");
+// }
+// let a = 20;
+// let b = 90;
+// let c = 60;
+ 
+// if(a >= b && a >= c){
+//     console.log(a);
+// } else if (b >= a && b >= c) {
+//     console.log(b);
+
+// }else {
+//     console.log(c);
+// }
+
 
 
 
